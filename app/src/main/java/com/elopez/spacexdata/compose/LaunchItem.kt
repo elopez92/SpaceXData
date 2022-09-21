f60a4228c14ebe1952dd5a7d77efc801349bc351
Eduardo <elopez2192@gmail.com>
2022-09-20 18:23:29 -0700

    Added highlight feature when selected a launch

--- app/src/main/java/com/elopez/spacexdata/compose/LaunchItem.kt
+++ app/src/main/java/com/elopez/spacexdata/compose/LaunchItem.kt
@@ -1,10 +1,7 @@
 package com.elopez.spacexdata.compose
 
 import android.util.Log
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -19,7 +16,10 @@ import androidx.compose.ui.unit.sp
 /*import coil.compose.AsyncImage
 import coil.request.ImageRequest*/
 import com.elopez.spacexdata.R
+import com.elopez.spacexdata.model.LaunchDataItem
 import com.elopez.spacexdata.utils.loadPicture
+import java.text.SimpleDateFormat
+import java.util.*
 
 @Composable
 fun LaunchItem(
@@ -29,25 +29,18 @@ fun LaunchItem(
     siteName: String,
     launchDate: String,
     imageString: String?,
-    onLaunchSelected: () -> Unit
+    selected: Boolean,
+    onLaunchSelected: (missionName: String) -> Unit
 ) {
     Row(
         modifier = modifier
             .fillMaxWidth()
             .border(border = BorderStroke(0.5.dp, Color.Gray))
-            .clickable { onLaunchSelected() },
+            .clickable { onLaunchSelected(missionName) }
+            .background(if(selected) Color.Yellow else Color.White),
         verticalAlignment = Alignment.CenterVertically,
     ) {
-        /*AsyncImage(
-            model = ImageRequest.Builder(LocalContext.current)
-                .data(image)
-                .crossfade(true)
-                .build(),
-            placeholder = painterResource(id = R.drawable.ic_rocket_black),
-            contentDescription = missionName,
-            contentScale = ContentScale.Crop
-        )*/
-        Log.i("Patch url", "LaunchItem: ${imageString}")
+        //Log.i("Patch url", "LaunchItem: ${launchDataItem.links.mission_patch_small ?: ""}")
         imageString.let {
             val image = loadPicture(url = it!!, defaultImage = R.drawable.ic_rocket_black).value
             image?.let{ img ->
@@ -71,7 +64,11 @@ fun LaunchItem(
             fontSize = 18.sp)
             Text(rocketName)
             Text(siteName)
-            Text(launchDate)
+            val date = Date(launchDate.toLong()?.times(1000L) ?: 0)
+            val sdf = SimpleDateFormat("dd-MM-yyyy HH:mm:ss z", Locale.ROOT)
+            sdf.timeZone = TimeZone.getDefault()
+            val formattedDate = sdf.format(date)
+            Text(formattedDate)
         }
     }
 
--- app/src/main/java/com/elopez/spacexdata/compose/LaunchList.kt
+++ app/src/main/java/com/elopez/spacexdata/compose/LaunchList.kt
@@ -5,8 +5,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.State
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import com.elopez.spacexdata.model.LaunchData
 import com.elopez.spacexdata.model.LaunchDataItem
@@ -18,6 +17,7 @@ fun LaunchList(
     modifier: Modifier = Modifier,
     onLaunchSelected: (LaunchDataItem) -> Unit
 ){
+    var selectedItem by remember{mutableStateOf("")}
     Box(modifier = modifier) {
         LazyColumn {
             itemsIndexed(items = list) { _, item ->
@@ -25,9 +25,11 @@ fun LaunchList(
                     missionName = item.mission_name ?: "",
                     rocketName = item.rocket?.rocket_name ?: "",
                     siteName = item.launch_site?.site_name ?: "",
-                    launchDate = item.launch_date_utc ?: "",
+                    launchDate = item.launch_date_unix?.toString() ?: "",
                     imageString = item.links?.mission_patch_small ?: "",
-                    onLaunchSelected = {onLaunchSelected(item)}
+                    selected = selectedItem == item.mission_name,
+                    onLaunchSelected = {onLaunchSelected(item)
+                    selectedItem = it}
                 )
             }
         }
